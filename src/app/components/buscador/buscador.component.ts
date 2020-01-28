9fb15dc251b7404b9d81c06d496be95d529b289d
Sebastian Echazú <52617032+SebastianEchazu@users.noreply.github.com>
2020-01-28 10:24:46 -0300

    Update buscador.component.ts

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -13,10 +13,10 @@ export class BuscadorComponent implements OnInit {
   androides: Androide[] = []
   termino: string;
 
-  constructor(private avtivatedRoute: ActivatedRoute, private router: Router, private _AndroidesService: AndroidesService) { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private _AndroidesService: AndroidesService) { }
 
   ngOnInit() {
-    this.avtivatedRoute.params.subscribe(params => {
+    this.activatedRoute.params.subscribe(params => {
       this.termino = params['termino'];
       this.androides = this._AndroidesService.buscarAndroide(params['termino']);
     });
