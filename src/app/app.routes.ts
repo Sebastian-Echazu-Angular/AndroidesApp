2946f7c40282e0d230b494c591fdf65443df7f87
sebastianechazu <sebastianechazu@gmail.com>
2024-05-18 04:02:33 +0000

    try to make a nuew angular version

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AndroidesComponent } from './components/androides/androides.component';
+import { AboutComponent } from './components/about/about.component';
+import { AndroideComponent } from './components/androide/androide.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'androides', component: AndroidesComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'androide/:id', component: AndroideComponent },
+    { path: 'buscar/:termino', component: BuscadorComponent },
+    //ruta de comodin en caso de no encontrar la direccion
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { AndroidesService } from './app/service/androides.service';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [AndroidesService]
+})
   .catch(err => console.error(err));
